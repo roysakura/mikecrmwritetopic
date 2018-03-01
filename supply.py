22c8497261c06d031214019ae8352015c859ae77
Roy <roy.liang@ectrend.com>
2018-03-01 14:41:24 +0800

    update

--- supply.py
+++ supply.py
@@ -6,7 +6,7 @@ import datetime
 
 today = datetime.date.today().strftime("%Y-%m-%d")
 print today
-filename = '磁易购【服务需求对接表】-表单反馈导出_%s.xlsx' % today
+filename = '磁易购【现货供应对接表】-表单反馈导出_%s.xlsx' % today
 wb = open_workbook(filename)
 ttfont = ImageFont.truetype("/Library/Fonts/微软雅黑.ttf",40)
 
@@ -16,12 +16,12 @@ for s in wb.sheets():
 		for col in range(s.ncols):
 			values.append(s.cell(row,col).value)
 		print values
-		im = Image.open("template.jpg")  
+		im = Image.open("template1.jpg")  
 		draw = ImageDraw.Draw(im)
 		draw.text((161,534),values[5], fill=(0,0,0),font=ttfont)  
 		draw.text((161,735),values[2], fill=(0,0,0),font=ttfont)
 		draw.text((161,935),values[7], fill=(0,0,0),font=ttfont)
 		draw.text((880,1700),values[0], fill=(0,0,0),font=ttfont)
-		save_filename = u'%s需求%s.jpg' % (values[2],values[0])
+		save_filename = u'%s供应%s.jpg' % (values[2],values[0])
 		im.save(save_filename)
 		values = []
